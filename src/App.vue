7d933bf5fa1f12d5aa19d94ae77c7685e6d9e15b
AmauriBlanco <blancoamauri@gmail.com>
2024-08-14 15:01:39 -0300

    2-2 Interpolação de dados

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
-<template>
+<script setup>
+const nome = "Amauri";
+const sobrenome = "Blanco";
+const idade = 33;
+</script>
 
+<template>
+    <p>{{`O nome do Professor é ${nome} ${sobrenome}, ele tem ${idade} anos`}}</p>
 </template>
