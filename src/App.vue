f9cf433703a7bea38658e1b7ca8fc643d438521b
AmauriBlanco <blancoamauri@gmail.com>
2024-08-19 15:13:28 -0300

    4-1 Iniciando com compnentes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
-import { ref } from "vue";
-
-const frutas = ref(["Maçã", "Banana", "Laranja"]);
-const novoItem = ref('')
-
-function adicionarNovoItem() {
-  frutas.value.push(novoItem.value)
-  novoItem.value = ''
-}
+  import Carro from "./components/Carro.vue"
 </script>
 
 <template>
-  <input @keyup.enter="adicionarNovoItem" v-model="novoItem" type="text" placeholder="Adicione um novo item">
-    <ul>
-        <li v-for="fruta in frutas" :key="fruta">
-            {{ fruta }}
-        </li>
-    </ul>
+  <Carro/>
+
+
 </template>
 
 <style scoped></style>
