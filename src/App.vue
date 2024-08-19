70c684a139727b9b8fe26a10248e04195d3771b4
AmauriBlanco <blancoamauri@gmail.com>
2024-08-19 15:53:46 -0300

    4-2 Passando props para o componente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <script setup>
-  import Carro from "./components/Carro.vue"
+import Carro from "./components/Carro.vue";
 </script>
 
 <template>
-  <Carro/>
-
-
+    <section class="carros">
+        <Carro
+            imagem="https://s2-autoesporte.glbimg.com/UWiWKJtlidgBWLaV7H-oeLuik7A=/0x0:620x400/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/s/k/6GMdJXSQqXVLy4BiL50g/2016-02-22-goldianteira.jpg"
+            nome="Gol"
+            ano="2017"
+            descricao="Carro Popular"
+        />
+        <Carro
+            imagem="https://www.autossegredos.com.br/wp-content/webp-express/webp-images/uploads/2015/06/palio_fire_0001.jpg.webp"
+            nome="Palio"
+            ano="2017"
+            descricao="Carro Popular"
+        />
+        <Carro
+            imagem="https://s2-autoesporte.glbimg.com/r04OvZkB5nl_WMm8UurxzGf2s-c=/0x0:620x400/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_cf9d035bf26b4646b105bd958f32089d/internal_photos/bs/2020/6/C/SmxefwT5Gf0ityb9lCng/2016-06-01-fiat-punto-2017.jpg"
+            nome="Punto"
+            ano="2017"
+            descricao="Carro Popular"
+        />
+    </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.carros {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+}
+</style>
--- src/components/Carro.vue
+++ src/components/Carro.vue
@@ -1,7 +1,29 @@
-<script setup></script>
+<script setup>
+const props = defineProps({
+    imagem: String,
+    nome: String,
+    ano: Number,
+    descricao: String,
+});
+</script>
+
 <template>
-    <h1>Carro</h1>
+    <div>
+        <img
+            :src="props.imagem"
+            alt=""
+            width="200px"
+            height="auto"
+        />
+        <h2> {{ props.nome }} </h2>
+        <h3>{{ props.ano }}</h3>
+        <p>{{ props.descricao }}</p>
+    </div>
 </template>
 <style scoped>
-
+h2,
+h3,
+p {
+    margin: 0 0 0.5rem 0;
+}
 </style>
